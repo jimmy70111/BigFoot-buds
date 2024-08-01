c8cddac62048c9f6feef2ece65dc34ece74f9946
JimmyJiang <tul70511@temple.edu>
2024-08-01 14:01:48 -0400

    first commit

---
+++ src/main/java/com/bigfoot/bigbuds/BigbudsApplication.java
@@ -0,0 +1,13 @@
+package com.bigfoot.bigbuds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigbudsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigbudsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigfoot/bigbuds/BigbudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigfoot.bigbuds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigbudsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
