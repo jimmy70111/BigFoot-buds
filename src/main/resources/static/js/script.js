dfaee792e1ad7228a9e86fe7125e705042efdcbf
JimmyJiang <tul70511@temple.edu>
2024-09-13 23:54:57 -0400

    fix img slider

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,25 +1,4 @@
-// JavaScript for Image Slider
-let currentIndex = 0; // This variable keeps track of the currently displayed image index.
-const images = document.querySelectorAll('.image-slider img'); // Select all the images in the slider
 
-function showImage(index) {
-    images.forEach((img, i) => {
-        img.classList.remove('active'); // Remove the 'active' class from all images
-        if (i === index) {
-            img.classList.add('active'); // Add the 'active' class to the current image
-        }
-    });
-}
-
-function nextImage() {
-    currentIndex = (currentIndex + 1) % images.length; // Increment the currentIndex and loop back to 0 if it exceeds the last image
-    showImage(currentIndex); // Call showImage to update the displayed image
-}
-
-function prevImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length; // Decrement the currentIndex and loop to the last image if it goes below 0
-    showImage(currentIndex); // Call showImage to update the displayed image
-}
 
 function toggle(event, contentClass) {
     // Get the node of specific content class name
